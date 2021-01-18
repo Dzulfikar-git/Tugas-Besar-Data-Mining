875805f7b2c9919c362bb8d913948385ea611bf5
Ahmad Adnan Shadiqin <76863588+ahmadadnan087@users.noreply.github.com>
2021-01-18 20:53:39 +0700

    Update Tugas Besar.R

--- Source Code/Tugas Besar.R
+++ Source Code/Tugas Besar.R
@@ -1,10 +1,17 @@
+#memasukkan work direktory
 work_directory <- "C:/Users/User/Documents/Semester 3/DM/TB"
 setwd(work_directory)
 dataset <- read.csv("data.csv", sep = ",")
 
+#uncomment baris dibawah jika belum menginstall packages yang akan digunakan
+#install.packages("C50")
+#install.packages("printr")
+
+#memanggil packages C50 dan printr
 library(C50)
 library(printr)
 
+#mengubah atribut pada dataset menjadi factor
 dataset$age <- as.factor(dataset$age)
 dataset$deliverynum <- as.factor(dataset$deliverynum)
 dataset$deliverytime <- as.factor(dataset$deliverytime)
@@ -12,13 +19,19 @@ dataset$bloodofpressure <- as.factor(dataset$bloodofpressure)
 dataset$heartproblem <- as.factor(dataset$heartproblem)
 dataset$caesarian <- as.factor(dataset$caesarian)
 
+#membuat model menggunakan algoritma C5.0  
 model <- C5.0(caesarian ~., data=dataset)
 model
-
+#menampilkan kesimpulan dari model yang dibuat
 summary(model)
+#menampilkan pohon keputusan dari model yang dibuat
 plot(model)
 
+#membuat objek bernama datatesting yang terdiri dari kolom 1-5 dari dataset
 datatesting <- dataset[1:5]
 
+#membuat prediksi dari model dan datatesting
 predictions <- predict(model, datatesting)
+
+#menampilkan tabel perbandingan dari hasil prediksi dengan nilai atribut caesarian 
 table(predictions, dataset$caesarian)
